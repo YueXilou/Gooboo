0f8702adc9fe9b70c1b7bf11b042ba3b73cc79ed
xilou_yue <134256492+YueXilou@users.noreply.github.com>
2024-07-31 13:23:41 +0800

    Add files via upload

--- cal.py
+++ cal.py
@@ -24,19 +24,21 @@ def calculator(cal_type, num_a, num_b):
 
 # initialization procedure
 empty_pack = tools.PicturePack(0)
-op_pack = tools.PicturePack(16)
+op_pack = tools.PicturePack(17)
 state_pack = tools.PicturePack(2)
-for i in range(16):
+for i in range(17):
     op_pack.load_picture(cv2.imread('./data/num'+str(i)+'.jpg'))
 for i in range(2):
     state_pack.load_picture(cv2.imread('./data/state_'+str(i)+'.jpg'))
-input_area = tools.Area(op_pack, [50, 215], 1800, 55, 0.7)
+input_area = tools.Area(op_pack, [50, 215], 1850, 55, 0.7)
 state_area = tools.Area(state_pack, [35, 157], 1443, 389, 0.8)
 
 time.sleep(2)
 
 # k-range can be changed.
-for k in range(10000):
+for k in range(1000000):
+
+    time.sleep(0.08)
 
     temp = tools.screenshot()
 
@@ -66,27 +68,57 @@ for k in range(10000):
                     flag = 1 - flag
                     cut_result.append(copy.deepcopy(cut_temp))
 
+        print(cut_result)
         left_num = ''
         right_num = ''
         op = 0
+        flag = 0
         for i in range(len(cut_result)):
             pict_temp = input_area.cut(temp)[:, cut_result[i][0]: cut_result[i][1]]
             # cv2.imshow('',pict_temp)
             # cv2.waitKey(0)
-            match_result = input_area.match(pict_temp, type=1)[0]
-            if match_result < 10:
-                if op == 0:
-                    left_num += str(match_result)
+            if cut_result[i][1] - cut_result[i][0] > 18:
+                match_result = input_area.match(pict_temp, type=1)[0]
+                if match_result < 10:
+                    if op == 0:
+                        left_num += str(match_result)
+                    else:
+                        right_num += str(match_result)
                 else:
-                    right_num += str(match_result)
+                    if match_result < 16 and flag == 0:
+                        op = match_result-9
+                        if left_num == '':
+                            left_num = '0'
+                    elif match_result == 16:
+                        flag = 1
+                        if op == 0:
+                            left_num += 'e'
+                        else:
+                            right_num += 'e'
+                    elif flag == 1:
+                        if op == 0:
+                            if match_result == 10:
+                                left_num += '+'
+                            else:
+                                left_num += '-'
+                        else:
+                            if match_result == 10:
+                                right_num += '+'
+                            else:
+                                right_num += '-'
+                        flag = 0
             else:
-                op = match_result-9
-                if left_num == '':
-                    left_num = '0'
-        left_num = int(left_num)
-        right_num = int(right_num)
+                if op == 0:
+                    left_num += '.'
+                else:
+                    right_num += '.'
+        left_num = int(float(left_num))
+        right_num = int(float(right_num))
         result = str(int(calculator(op, left_num, right_num)))
 
+        print(left_num,right_num,result)
+
+        gui.PAUSE = 0.0005
         for i in range(len(result)):
             gui.press(result[i])
         gui.press('enter')
